9b97c79f191fa7873052327ebe34f6d946a45e36
singh.kulwant@gmx.com <singh.kulwant@gmx.com>
2020-04-11 01:40:21 +0530

    10-04: initial commit

---
+++ src/main/java/com/jms/SpringJmsAppApplication.java
@@ -0,0 +1,13 @@
+package com.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJmsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJmsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jms/SpringJmsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJmsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
